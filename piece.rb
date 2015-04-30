447470f7fad2fccd138cf4fb1b608c6978940134
Alex Chebotarev <aleksey.chebotarev@gmail.com>
2015-04-30 11:37:54 -0700

    Started implementing Piece movement logic

--- piece.rb
+++ piece.rb
@@ -1,2 +1,31 @@
 class Piece
+  attr_reader :color, :kinged
+
+  def initialize(params)
+    @board = params[:board]
+    @pos = params[:pos]
+    @color = params[:color]
+    @kinged = false
+  end
+
+  def perform_slide(end_pos)
+    if move_diffs.include?(end_pos)
+  end
+
+  def perform_jump
+
+  end
+
+  def move_diffs
+    if kinged
+
+    else
+      modifier = color == :white ? 1 : -1
+      [[modifier * 1, 1], [modifier * 1,-1]]
+    end
+  end
+
+  def symbol
+    '☻'
+  end
 end
